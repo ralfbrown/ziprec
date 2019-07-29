73f6c99eb2e1d8ec86fa09bac333235d87bc9ee7
ralfbrown <ralfbrown@users.noreply.github.com>
2019-07-29 17:28:43 -0400

    convert raw  pointer to smart pointer

--- loclist.h
+++ loclist.h
@@ -5,7 +5,7 @@
 /*									*/
 /*  File: loclist.h - stream location references			*/
 /*  Version:  1.10beta				       			*/
-/*  LastEdit: 2019-07-26						*/
+/*  LastEdit: 2019-07-29						*/
 /*									*/
 /*  (c) Copyright 2011,2012,2013,2019 Carnegie Mellon University	*/
 /*      This program is free software; you can redistribute it and/or   */
@@ -27,6 +27,7 @@
 #define __LOCLIST_H_INCLUDED
 
 #include <sys/types.h>
+#include "framepac/smartptr.h"
 #include "framepac/memory.h"
 
 /************************************************************************/
@@ -86,13 +87,14 @@ class LocationList
    public:
       void *operator new(size_t) { return allocator->allocate() ; }
       void operator delete(void *blk) { allocator->release(blk) ; }
+      LocationList() : m_offset(0), m_sigtype(ST_Invalid) {}
       LocationList(SignatureType st, off_t offset, LocationList *nxt = nullptr)
-	 { m_offset = offset ; m_sigtype = st ; m_next = nxt ; }
-      ~LocationList() { if (m_next) delete m_next ; m_next = nullptr ; }
+	 : m_next(nxt), m_offset(offset), m_sigtype(st) {}
+      ~LocationList() = default ;
 
       // accessors
-      const LocationList *next() const { return m_next ; }
-      LocationList *next() { return m_next ; }
+      const LocationList* next() const { return m_next ; }
+      LocationList* next() { return m_next.get() ; }
       SignatureType signatureType() const { return m_sigtype ; }
       off_t offset() const { return m_offset ; }
       off_t headerEndOffset(const char *buffer, bool zip64 = false) const ;
@@ -104,8 +106,8 @@ class LocationList
       LocationList *reverse() ;
 
    private:
-      static Fr::SmallAlloc* allocator ;
-      LocationList *m_next ;
+      static Fr::SmallAlloc*  allocator ;
+      Fr::Owned<LocationList> m_next ;
       off_t m_offset ;
       SignatureType m_sigtype ;
    } ;
